b5a277035dd0cdef06a34ae15224a5b61b2d5e8a
rbverissimo <rverissimo.dev@gmail.com>
2023-03-13 20:52:58 -0300

    Looking at some basics at Types, Union Types and Type Inference in TS

---
+++ basics.ts
@@ -0,0 +1,70 @@
+// Primitives: number, string, boolean
+// More complex types: array, objects
+//Function types, parameters
+
+//Primitives
+
+let age: number;
+age = 12;
+// can't be like age = '12';
+
+let userName: string;
+userName = "Renato";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+//Arrays and objects
+
+let hobbies: string[];
+//boolean[], number[]
+
+hobbies = ["Sports", "Cooking", "Something else"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Renato",
+  age: 32,
+};
+
+/* person = {
+  isEmployee: true,
+}; */
+
+let people: {
+  name: string;
+  age: number;
+}[]; /* this is also possible and means an array of objects */
+
+// Type inference --- GOOD PRACTICE, EMBRACE IT
+let course = "React - The Complete Guide";
+/* course = 12345;   no can do because of type inference */
+
+/* UNION TYPES */
+
+let someTypes: string | number = "Something";
+someTypes = 1; /* it is okey */
+
+// Type Alias
+type Person = {
+  name: string;
+  age: number;
+};
+
+// this variable is of the type array of Person
+let lotsOfDudes: Person[];
+
+//Functions & Types
+
+function addSomething(a: number, b: number) {
+  return a + b;
+}
+
+/* VOID RETURN */
+function printSomething(value: any) {
+  console.log(value);
+}
